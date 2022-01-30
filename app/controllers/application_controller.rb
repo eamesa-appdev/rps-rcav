01cfe51be8a25b475c8a2488550c88f0d03383df
eamesa <eamadorm@chicagobooth.edu>
2022-01-30 21:13:13 +0000

    RCAV 100% Target 100%

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,33 +2,50 @@ class ApplicationController < ActionController::Base
   layout(false)
   # Add your actions below this line
   # ================================
-  def play_rock
-    render ({ :template => "game_templates/user_rock.html.erb"})
-
-    #render ({ :html => "<h1>Hello, World!</h1>".html_safe})
-    #redirect_to("https://en.wikipedia.org/wiki/Rock")
+  def homepage
+   render ({ :template => "game_templates/rules.html.erb", :layout => "wrapper.html.erb"})
   end
 
-  def homepage
-   render ({ :template => "game_templates/rules.html.erb"})
+  def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+    
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end 
+
+    render ({ :template => "game_templates/user_rock.html.erb",:layout => "wrapper.html.erb"})
   end
 
   def play_scissors
-    render ({ :template => "game_templates/user_scissors.html.erb"})
+    @comp_move = ["rock", "paper", "scissors"].sample
+    
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end 
+
+    render ({ :template => "game_templates/user_scissors.html.erb",:layout => "wrapper.html.erb"})
   end
 
   def play_paper
     @comp_move = ["rock", "paper", "scissors"].sample
     
     if @comp_move == "rock"
-    outcome = "won"
+    @outcome = "won"
     elsif @comp_move == "paper"
-    outcome = "tied"
+    @outcome = "tied"
     elsif @comp_move == "scissors"
-    outcome = "lost"
+    @outcome = "lost"
     end
-    
-    render ({ :template => "game_templates/user_paper.html.erb"})
+
+    render ({ :template => "game_templates/user_paper.html.erb", :layout => "wrapper.html.erb"})
   end
 
 end
