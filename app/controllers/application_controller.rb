6320e9adcb2e08040b7a84f4e5ba2274b6ae4997
eamesa <eamadorm@chicagobooth.edu>
2022-01-30 19:49:14 +0000

    RCAV embedded ruby

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,19 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
   def play_rock
-    redirect_to("https://www.wikipedia.org")
+    render ({ :template => "game_templates/user_rock.html.erb"})
+
+    #render ({ :html => "<h1>Hello, World!</h1>".html_safe})
+    #redirect_to("https://en.wikipedia.org/wiki/Rock")
   end
+
+  def play_paper
+    redirect_to("https://www.wikipedia.org/wiki/Paper")
+  end
+
+  def play_scissors
+    redirect_to("https://www.wikipedia.org/wiki/Scissors")
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/rock", { :controller => "application", :action => "play_rock"})
 end
+
+Rails.application.routes.draw do
+  get("/paper", { :controller => "application", :action => "play_paper"})
+end
+
+Rails.application.routes.draw do
+  get("/scissors", { :controller => "application", :action => "play_scissors"})
+end
