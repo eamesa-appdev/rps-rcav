7c3e7f3d3454a56da18d52d6bd03e30593f3d3e0
eamesa <eamadorm@chicagobooth.edu>
2022-01-30 20:29:49 +0000

    paper defined application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,22 @@ class ApplicationController < ActionController::Base
    render ({ :template => "game_templates/rules.html.erb"})
   end
 
-  def play_paper
-    render ({ :template => "game_templates/user_paper.html.erb"})
-  end
-
   def play_scissors
     render ({ :template => "game_templates/user_scissors.html.erb"})
   end
 
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+    
+    if @comp_move == "rock"
+    outcome = "won"
+    elsif @comp_move == "paper"
+    outcome = "tied"
+    elsif @comp_move == "scissors"
+    outcome = "lost"
+    end
+    
+    render ({ :template => "game_templates/user_paper.html.erb"})
+  end
 
 end
