73025373aa910e8e57fbfd9cd1654fbf90a57078
eamesa <eamadorm@chicagobooth.edu>
2022-01-30 19:23:42 +0000

    RCAV redirect to wiki

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   layout(false)
-
   # Add your actions below this line
   # ================================
+  def play_rock
+    redirect_to("https://www.wikipedia.org")
+  end
 
 end
